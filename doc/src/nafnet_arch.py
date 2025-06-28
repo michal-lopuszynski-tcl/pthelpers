f9b24bc2b70d600ab7fd092d874d4a1c67512d60
Michal Lopuszynski <michal.lopuszynski@tcl.com>
2025-06-28 23:01:35 +0200

    [doc] Clean-up sample architecture

--- doc/src/nafnet_arch.py
+++ doc/src/nafnet_arch.py
@@ -1,3 +1,7 @@
+# Included for demonstration purposes. Adapted from:
+#
+# https://github.com/megvii-research/NAFNet
+#
 # ------------------------------------------------------------------------
 # Copyright (c) 2022 megvii-model. All Rights Reserved.
 # ------------------------------------------------------------------------
@@ -14,48 +18,6 @@ Simple Baselines for Image Restoration
 """
 
 import torch
-
-
-class LayerNormFunction(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x, weight, bias, eps):
-        ctx.eps = eps
-        N, C, H, W = x.shape
-        mu = x.mean(1, keepdim=True)
-        var = (x - mu).pow(2).mean(1, keepdim=True)
-        y = (x - mu) / (var + eps).sqrt()
-        ctx.save_for_backward(y, var, weight)
-        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
-        return y
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        eps = ctx.eps
-
-        N, C, H, W = grad_output.size()
-        y, var, weight = ctx.saved_variables
-        g = grad_output * weight.view(1, C, 1, 1)
-        mean_g = g.mean(dim=1, keepdim=True)
-
-        mean_gy = (g * y).mean(dim=1, keepdim=True)
-        gx = 1.0 / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
-        return (
-            gx,
-            (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0),
-            grad_output.sum(dim=3).sum(dim=2).sum(dim=0),
-            None,
-        )
-
-
-class LayerNorm2d(torch.nn.Module):
-    def __init__(self, channels, eps=1e-6):
-        super(LayerNorm2d, self).__init__()
-        self.register_parameter("weight", torch.nn.Parameter(torch.ones(channels)))
-        self.register_parameter("bias", torch.nn.Parameter(torch.zeros(channels)))
-        self.eps = eps
-
-    def forward(self, x):
-        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
 
 
 class DyT(torch.nn.Module):
